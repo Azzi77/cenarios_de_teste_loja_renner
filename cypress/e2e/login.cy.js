ef27c928a49dfe01efb550e5c6959b79082fc3ec
Regina Azzi <reginaazzi@hotmail.com>
2023-01-18 10:52:39 -0300

    primeiro teste

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,6 +1,8 @@
-describe('template spec', () => {
-  it('passes', () => {
+
+
+describe('Aceitar termos de uso do site', () => {
+  it('Deve acessar o site e clicar e aceitar termos de uso', () => {
     cy.visit('https://www.lojasrenner.com.br/')
-    cy.contains('Entre ou cadastre-se').click()
+    cy.contains('Li e aceito as Politicas de Privacidade e Termos de Uso.').click()
   })
 })
